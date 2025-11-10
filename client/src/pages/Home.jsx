a7416ec040b08a121d7dc8fc85bc152055012c4e
Betina9 <ob042025@gmail.com>
2025-11-10 14:38:55 +0100

    lagrer forløbig

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <h1>Welcome to Makeup Dream</h1>;
+  return <h1>Glow Into Summer With Makeup Dream</h1>;
+  <SwiperSlide>
+          <img src="/images/summer1.png" alt="Summer Makeup" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/summerflower.png" alt="Summer Flower" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/makeup.png" alt="Makeup" />
+        </SwiperSlide>
+      </Swiper>
+    </main>
+  );
 }
